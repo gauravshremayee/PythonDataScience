064aa0a310182bb2cbefa4a88506cb6725892f58
Gaurav <gaurav.2897@gmail.com>
2019-10-19 01:11:56 +0530

    Update numpyReshape.py

--- numpyReshape.py
+++ numpyReshape.py
@@ -4,5 +4,26 @@ import numpy as np
 #2*2 wont work as there are 9 elements ,so accomodating in 2*2 would be too less space for 9 elements
 grid = np.arange(1, 10).reshape((3, 3)) 
 print(grid)
+#output
+#[[1 2 3]
+# [4 5 6]
+# [7 8 9]]
+
+x = np.array([1, 2, 3]) # row vector via reshape
+x.reshape((1, 3))
+
+print (x[np.newaxis, :])
+
+# output [[1 2 3]]
+
+# column vector via reshape 
+
+x.reshape((3, 1))
+print ( x[:, np.newaxis])
+
+#output 
+#[[1]
+#[2]
+#[3]]
 
 
