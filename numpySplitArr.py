7d6fb18f684246b5f491597c1dc54daac0655e65
Gaurav <gaurav.2897@gmail.com>
2019-10-19 01:40:32 +0530

    Update numpySplitArr.py

--- numpySplitArr.py
+++ numpySplitArr.py
@@ -1,4 +1,25 @@
 
+#np.split(arrayName, num)
+#num times split
+
+#np.split(arrayName,[num1,num2])
+
+#split first num1 elements second subarray num1 to num2 and then remaining print as it is
+# 1 2 3 4 5 6 7 8 9 10
+#np.split(arr,[3,9]) means  (1,2,3) (4,5,6,7,8,9) and then remaining is (10)
+x = np.arange(9)
+
+#split 3 elements each 
+print(np.split(x, 3))
+
+#[array([0, 1, 2]), array([3, 4, 5]), array([6, 7, 8])]
+
+#split first 3 and then 3 to 7 and then remaining print as it is
+print(np.split(x,[3,7]))
+#[array([0, 1, 2]), array([3, 4, 5, 6]), array([7, 8])]
+                         
+
+
 x=[1,2,3,99,99,3,2,1] 
 x1, x2, x3 = np.split(x, [3, 5])
 print(x1, x2, x3) 
