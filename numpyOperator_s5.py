d30f5a16e6798a2c4e0c8ff2bfeb94e20f1d6ad3
Gaurav <gaurav.2897@gmail.com>
2019-10-19 20:52:33 +0530

    Update numpyOperator_s5.py

--- numpyOperator_s5.py
+++ numpyOperator_s5.py
@@ -64,7 +64,7 @@ print(y)
 
 print ("min and max of array is:")
 
-print ( np.min(big_array), np.max(big_array)) 
+print ( np.min(x), np.max(x)) 
 
 print (np.subtract(x,2))
 #[-2 -1  0  1  2  3  4  5  6]
